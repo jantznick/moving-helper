f55e9c59a1a84e48ac57b0bf6df1843fb9f22407
Nick Jantz <thenickjantz@gmail.com>
2020-03-18 22:55:25 -0500

    make port uppercase PORT

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 require('dotenv').config()
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine","ejs");
 app.use(express.static("public"));
